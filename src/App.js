c58dcf3a3c3faf3ccd91b67b04fa87522d13fea5
Andrew Hansbrough <hansbroughku@gmail.com>
2020-08-06 10:24:42 -0500

    Testing routes. Nav shows up but no pages

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Router>
-        <Nav />
         <Box>
           <Switch>
             <Route exact path={["/", "/main"]}>
