799d5f2be4f44cc6799f4165ea8a179b51e38792
Andrew Hansbrough <hansbroughku@gmail.com>
2020-08-05 21:26:25 -0500

    Created Main page component, contains header and footer, body.

---
+++ src/pages/Main.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+import StickyFooter from "../components/Footer/Footer";
+import Link from "@material-ui/core/Link";
+import Bio from "../components/Bio/Bio";
+import backdrop from "../components/Backdrop/7150.jpg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.primary,
+    background: "transparent"
+  },
+
+  backdrop: {
+    backgroundImage: `url(${backdrop})`
+  }
+}));
+
+export default function CenteredGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <Paper className={(classes.paper, classes.backdrop)}>
+            <Grid container spacing={3}>
+              <Grid item xs={3}></Grid>
+              <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                <Paper className={classes.paper}>
+                  <Bio />
+                </Paper>
+              </Grid>
+              <Grid item xs={3}></Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+      <StickyFooter />
+    </div>
+  );
+}
