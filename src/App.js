d9d43151ecce73d882333fbf5bdca1fd9ae926f6
Andrew Hansbrough <hansbroughku@gmail.com>
2020-08-05 21:22:03 -0500

    Added Ruotes to contact and portfolio using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import Main from "./pages/Main.js";
+import Contact from "./pages/Contact.js";
+import Portfolio from "./pages/Portfolio.js";
+import Nav from "./components/Nav/Nav.js";
+import Box from "@material-ui/core/Box";
+// import theme from './components/Theme/theme'
+// import { ThemeProvider } from '@material-ui/core/styles';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        {/* <ThemeProvider>
+          <CSSBaseline /> */}
+        <Box>
+          <Switch>
+            <Route exact path={["/", "/main"]}>
+              <Main />
+            </Route>
+            <Route exact path={["/contact"]}>
+              <Contact />
+            </Route>
+            <Route exact path={["/portfolio"]}>
+              <Portfolio />
+            </Route>
+          </Switch>
+        </Box>
+        {/* </ThemeProvider> */}
+      </Router>
     </div>
   );
 }
