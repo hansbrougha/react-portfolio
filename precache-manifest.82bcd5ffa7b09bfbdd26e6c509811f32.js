7b0a29bc2e995c7ede224da58370bf936843c626
Andrew Hansbrough <hansbroughku@gmail.com>
2020-08-06 10:07:14 -0500

    Updates

---
+++ precache-manifest.82bcd5ffa7b09bfbdd26e6c509811f32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ffa1f4624699ccea3b2541f87bf9ea",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d4c85ef9eaf6c240b185",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ac0beffa13c0ac74137",
+    "url": "/react-portfolio/static/js/2.01df6f4a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-portfolio/static/js/2.01df6f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c85ef9eaf6c240b185",
+    "url": "/react-portfolio/static/js/main.33e33a58.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "20703ed329f67ed0ae53634b65cb17cf",
+    "url": "/react-portfolio/static/media/7150.20703ed3.jpg"
+  }
+]);
