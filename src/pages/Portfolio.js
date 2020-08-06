8d0f51eda44ee39f1fb767c1c12f23a018ae7ab4
Andrew Hansbrough <hansbroughku@gmail.com>
2020-08-05 21:28:50 -0500

    Created Portfolio gallery containing 8 projects on cards. Info is imported from ghapps.json

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import MediaCard from "../components/Card/Card";
+import StickyFooter from "../components/Footer/Footer";
+import ghapps from "../ghapps.json";
+import backdrop from "../components/Backdrop/7150.jpg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  },
+  backdrop: {
+    backgroundImage: `url(${backdrop})`
+  }
+}));
+
+export default function CenteredGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <Paper className={(classes.paper, classes.backdrop)}>
+            <Grid container spacing={3}>
+              {ghapps.map((ghapps) => (
+                <MediaCard
+                  id={ghapps.id}
+                  key={ghapps.id}
+                  name={ghapps.name}
+                  image={ghapps.image}
+                  description={ghapps.description}
+                  href={ghapps.sourceCode}
+                  url={ghapps.url}
+                />
+              ))}
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+      <StickyFooter />
+    </div>
+  );
+}
