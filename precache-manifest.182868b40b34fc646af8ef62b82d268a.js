19f47e6841d8737d87bf03f3cdfd33f85732535b
Andrew Hansbrough <hansbroughku@gmail.com>
2020-08-07 00:16:34 -0500

    Updates

---
+++ precache-manifest.182868b40b34fc646af8ef62b82d268a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb17737e6cf092ee86e2d1b2139d76e",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ab8fd2b26db4361d3077",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc1adf8451d856cc0fc6",
+    "url": "/react-portfolio/static/js/2.cae3dd8f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-portfolio/static/js/2.cae3dd8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8fd2b26db4361d3077",
+    "url": "/react-portfolio/static/js/main.c102989c.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "20703ed329f67ed0ae53634b65cb17cf",
+    "url": "/react-portfolio/static/media/7150.20703ed3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/main/precache-manifest.c8bf700a5ae58962f185f8e162adebb3.js"
+  "/react-portfolio/precache-manifest.182868b40b34fc646af8ef62b82d268a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/main/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
